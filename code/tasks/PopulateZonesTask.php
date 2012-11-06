9b4e756a933e5ed3f7504c29975e50c917cc6143
Jeremy Shipman <jeremy@burnbright.net>
2012-11-06 16:29:38 +1300

    NEW: modified PopulateZonesTask to allow deleting all Zones and ZonePrices

--- code/tasks/PopulateZonesTask.php
+++ code/tasks/PopulateZonesTask.php
@@ -3,8 +3,23 @@
 class PopulateZonesTask extends BuildTask{
 
 	function run($request){
+		if($request->getVar('clear')){
+			$this->clear();
+		}else{
+			$this->populate();
+		}
+	}
+	
+	function populate(){
 		$fixture = new YamlFixture("shop_zonedpricing/tests/fixtures/zonedpricing.yml");
 		$fixture->saveIntoDatabase();
+		echo "created zones, and example product.";
+	}
+	
+	function clear(){
+		DB::query("DELETE FROM \"Zone\" WHERE 1;");
+		DB::query("DELETE FROM \"ZonePrice\" WHERE 1;");
+		echo "Deleted all zones and zoneprices";
 	}
 	
 }
