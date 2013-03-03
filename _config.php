dd48e790fcae4a1d70df4b5426884996862f0cf4
Jeremy Shipman <jeremy@burnbright.net>
2013-03-03 14:43:31 +1300

    Removed ZonedRegionRestriction decorator, as we can use ZoneRegion instead

--- _config.php
+++ _config.php
@@ -1,7 +1,7 @@
 <?php
 
 Object::add_extension("Product", "ZonedPricingProductDecorator");
-Object::add_extension("RegionRestriction", "ZonedRegionRestriction");
+
 Object::add_extension("Page_Controller", "LocationFormPageDecorator");
 Object::add_extension("ShopUserInfo","ZoneUserInfo");
 
